2c86012d1a429827af7084ffb634435d515b6c7f
Michal Zukowski <mikemix@users.noreply.github.com>
2017-08-22 23:17:24 +0200

    Placeholder

--- modules/BootstrapIntegration/src/View/Helper/FormElement.php
+++ modules/BootstrapIntegration/src/View/Helper/FormElement.php
@@ -2,6 +2,8 @@
 
 namespace BootstrapIntegration\View\Helper;
 
+use Zend\Form\Element\Email;
+use Zend\Form\Element\Text;
 use Zend\Form\ElementInterface;
 
 final class FormElement extends \Zend\Form\View\Helper\FormElement
@@ -11,6 +13,12 @@ final class FormElement extends \Zend\Form\View\Helper\FormElement
         $element
             ->setAttribute('class', trim($element->getAttribute('class') . ' form-control'))
             ->setAttribute('id', $element->getAttribute('id') ?: 'element_' . md5(uniqid(microtime(), true)));
+
+        if ($element->getLabel() && empty($element->getAttribute('placeholder') &&
+            ($element instanceof Text || $element instanceof Email))
+        ) {
+            $element->setAttribute('placeholder', $element->getLabel());
+        }
 
         return parent::render($element);
     }
